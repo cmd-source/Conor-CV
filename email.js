8a0278ab641c0767e0b62d400ae90a47aa53e087
cmd-source <conorcrosbie09@gmail.com>
2021-03-03 20:53:16 +0000

    creating a function for the send email, revisit in the morning

--- email.js
+++ email.js
@@ -1,1 +1,14 @@
-console.log("hello")
+function sendMail(contactForm) {
+    email.send("gmail", "template_7qtjd0s", {
+        "from_name": contactForm.name.value,
+        "from_email": contactFrom.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("success", respponse);
+        },
+        function(error) {
+            console.log("failed, error");
+        })
+}
