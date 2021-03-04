a44e6a67f1b85ddc98321542a9739aea830edf20
cmd-source <conorcrosbie09@gmail.com>
2021-03-04 17:38:45 +0000

    trying to find a fix for the js email

--- email.js
+++ email.js
@@ -1,17 +1,16 @@
 function sendMail(contactForm) {
-    email.send("gmail", "template_7qtjd0s", {
+    email.send("gmail", "conor", {
         "from_name": contactForm.name.value,
         "from_email": contactFrom.emailaddress.value,
         "project_request": contactForm.projectsummary.value
     })
-    .then(
-        function(response) {
-            console.log("success", response);
-        },
-        function(error) {
-            console.log("failed", error);
-        }
-    );
+        .then(
+            function (response) {
+                console.log("success", response);
+            },
+            function (error) {
+                console.log("failed", error);
+            }
+        );
     return false;
 }
-
